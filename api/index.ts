46b6c4ef90303851998fa624ee03bde3d25853d3
Shane Avery Sistoza <me@ssistoza.dev>
2020-10-26 13:46:50 -0400

    test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 const avatarsMiddleware = require('adorable-avatars');
 const app = require('express')();
 
-app.use('/api', avatarsMiddleware)
+app.use('/api', avatarsMiddleware);
 
 module.exports = app;
